eb7a37cdf5f4ae07b8642341cb93562b09c30ba8
Kamal Allouzi <31010418+kamalallouzi@users.noreply.github.com>
2021-03-03 03:52:02 -0600

    Add files via upload

--- discordRichGroove.py
+++ discordRichGroove.py
@@ -2,19 +2,19 @@ from __future__ import print_function
 from pypresence import Presence
 from time import time
 from time import sleep
-from os import popen
-from os import name
-from os import system
-from psutil import Process
+import os
+import psutil
 import audio_metadata
 RPC = Presence(805717663975473162)
 RPC.connect()
 def cls():
-    system('cls' if name=='nt' else 'clear')
+    os.system('cls' if os.name=='nt' else 'clear')
 try:
-    pid = popen('wmic process get description, processid').read()
-    pid = int(pid[pid.index("Music.UI.exe") + 12: pid.index("Music.UI.exe") + 50])
-    p = Process(pid)
+    pid = os.popen('wmic process get description, processid').read()
+    pid = int(pid[pid.index("Music.UI.exe") + 12: pid.index("Music.UI.exe") + 45])
+    print(pid)
+    p = psutil.Process(pid)
+    print(p)
     openHandles = p.open_files()
     matching = [s for s in openHandles if "\Music" in str(s)]
     music = str(matching[0])
@@ -23,16 +23,16 @@ try:
     metadata = audio_metadata.load(info)
     infoTemp = info
     metadata = audio_metadata.load(info)
-    RPC.update(pid=10276, state=metadata.tags['artist'][0], details=metadata.tags['title'][0], large_image="groove", small_image="mad", large_text="Groove Music", small_text="madge", end=int(time())+int(metadata.streaminfo['duration']))
+    RPC.update(state=metadata.tags['artist'][0], details=metadata.tags['title'][0], large_image="groove", small_image="mad", large_text="Groove Music", small_text="madge", end=int(time())+int(metadata.streaminfo['duration']))
 except ValueError:
     print("Groove Music not running")
 except IndexError: 
-    RPC.update(pid=10276, state="Idle", details="Idle", large_image="groove", small_image="mad", large_text="Groove Music", small_text="madge")
+    RPC.update(state="Idle", details="Idle", large_image="groove", small_image="mad", large_text="Groove Music", small_text="madge")
 while True:
     try:
-        pid = popen('wmic process get description, processid').read()
-        pid = int(pid[pid.index("Music.UI.exe") + 12: pid.index("Music.UI.exe") + 50])
-        p = Process(pid)
+        pid = os.popen('wmic process get description, processid').read()
+        pid = int(pid[pid.index("Music.UI.exe") + 12: pid.index("Music.UI.exe") + 45])
+        p = psutil.Process(pid)
         openHandles = p.open_files()
         matching = [s for s in openHandles if "\Music" in str(s)]
         music = str(matching[0])
